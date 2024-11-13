ce9d9f7ae26c939a0e97cfe7e7b43339525fef41
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2024-11-13 18:57:44 +0100

    a

--- zwa/1v1/ships/script.js
+++ zwa/1v1/ships/script.js
@@ -1,15 +1,18 @@
-/*
 const connection = new WebSocket("ws://localhost:8080");
 
 var ME = {
     ships: [],
-    id: Math.floor(Math.random()*1000)
+    id: Math.floor(Math.random()*1000),
+    hover: ['',''],
+    bombs: []
 }
 var OP = {
     ships: [],
-    id: 0
+    id: 0,
+    hover: ['',''],
+    bombs: []
 }
-
+console.log(ME.id)
 connection.onopen = () => {
     console.log("WebSocket is open now.");
 };
@@ -27,11 +30,46 @@ connection.onmessage = (event) => {
     const data = JSON.parse(event.data.toString());
 
     if(data.id != ME.id) OP = data
+
+    if(ME.ships.length >= 10 && OP.ships.length >= 10){
+        document.getElementById('info').style.opacity = '0'
+    }
+
+    for(let i of OP.bombs){
+        document.getElementById(`cell${i[0]}-${i[1]}`).style.backgroundColor = 'black'
+    }
+    for(let i of ME.bombs){
+        document.getElementById(`img${i[0]}-${i[1]}-o`).src = 'assets\\cross.png'
+    }
+    console.log(OP)
 }
-*/
+function sendData(){
+    
+    const data = ME
+
+    try{
+        connection.send(JSON.stringify(data));
+    }catch(error){}
+
+}
 function resetId(){
-    // ME.id = Math.floor(Math.random()*1000)
+    ME.id = Math.floor(Math.random()*1000)
     console.log(ME.id)
+}
+function includesArr(arrToSearch, arr){
+
+    for(let i of arrToSearch){
+        if(JSON.stringify(i) == JSON.stringify(arr)) return true
+    }
+    return false
+}
+function victory(){
+    let counter = 0
+    for(let i of ME.ships){
+        if(includesArr(OP.bombs, i)) counter++        
+    }
+    if(counter >= 10) return true
+    return false
 }
 function generateTable(tableId){
 
@@ -43,20 +81,43 @@ function generateTable(tableId){
         for(let x = 0; x<10; x++){
 
             let cell = document.createElement('td')
-            cell.id = `${x}-${y}`
+            let div = document.createElement('div')
+            let img = document.createElement('img')
+            
+            img.id = tableId == 'myTable' ? `img${x}-${y}` : `img${x}-${y}-o`
+            div.id = tableId == 'myTable' ? `cell${x}-${y}`: `cell${x}-${y}-o`
 
-            cell.addEventListener('mousedown', e => {
-                if(e.button == 2 && ME.ships.length < 10){
-                    ME.ships.push([x, y])
-                    console.log(ME.ships)
-                }
-            })
-            if(tableId == 'opTable'){
-                cell.addEventListener('mouseover', () => {
+            if(tableId == 'myTable'){
+                div.addEventListener('mousedown', e => {
+            
+                    if(e.button == 0 && ME.ships.length < 10 && !includesArr(ME.ships, [x, y])){
+            
+                        ME.ships.push([x, y])
+                        img.src = 'assets\\ship.png'
+                    }
+                })
+            }else{
+                div.addEventListener('mousedown', e => {
+                    
+                    if(e.button == 0 && ME.ships.length == 10 && OP.ships.length == 10){
 
+                        ME.bombs.push([x, y])
+                    }
                 })
             }
 
+            div.addEventListener('mouseover', () => {
+                div.style.boxShadow = 'inset 5px 5px 0 white, inset -5px -5px 0 white'
+                if(tableId == 'myTable') ME.hover[0] = div.id
+                else ME.hover[1] = div.id
+            })
+            div.addEventListener('mouseleave', () => {
+                div.style.boxShadow = ''
+                if(ME.hover[0] == div.id) ME.hover[0] = ''
+                else if(ME.hover[1] == div.id) ME.hover[1] = ''
+            })
+            div.appendChild(img)
+            cell.appendChild(div)
             row.appendChild(cell)
         }
 
@@ -65,6 +126,8 @@ function generateTable(tableId){
 }
 generateTable('myTable')
 generateTable('opTable')
+
+window.addEventListener('mouseup', sendData)
 
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
