2398802f93debefac595023980da92cd6865b68a
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2024-12-05 08:36:22 +0100

    A

--- zda/sql/autoServis.sql
+++ zda/sql/autoServis.sql
@@ -12,14 +12,14 @@ create table AUTA(
     SPZ varchar(255) not null,
     model varchar(255) not null,
     rokVyroby date not null,
-    zakaznik counter(16) not null,
+    zakaznik int not null,
     foreign key (zakaznik) references ZAKAZNICI (idZakaznika)
 );
 
 create table OPRAVY(
 
     idOpravy counter(16) not null primary key,
-    popis varchar(512) not null,
+    popis varchar(255) not null,
     cena int not null,
     datum date not null,
     idAuta int not null,
@@ -32,7 +32,6 @@ create table ZAMESTNANCI(
     jmeno varchar(255) not null,
     prijmeni varchar(255) not null,
     mzda int,
-    oprava int,
 );
 
 create table SPOJENI(
@@ -44,10 +43,32 @@ create table SPOJENI(
 );
 insert into ZAKAZNICI(jmeno, prijmeni, telefon)
 values
-("David", "Mastny","123456789"),
-("Honza","Cervenka","198756148"),
-("Jan","Zelezny","789361377"),
-("Karel","Barel","123789156"),
+("David", "Mastny","123456789");
+("Honza","Cervenka","198756148");
+("Jan","Zelezny","789361377");
+("Karel","Barel","123789156");
 ("Anicka","Tkanicka","734897165");
 
-insert into ZAMESTNANCI values(Dratenka, KutilAOpravar, 100);
+insert into ZAMESTNANCI(jmeno, prijmeni, mzda)
+values
+("Dratenka", "KutilAOpravar", 100);
+("Franta","Švanda",500);
+("Dijana","Raklotvorna",-500);
+("LePan","Tau",1000);
+("Vodník","Nemazbrojnik",400);
+
+insert into AUTA(SPZ, model, rokVyroby, zakaznik)
+values
+("1TF8568", "polednik", "01.15.1991", 8);
+("1H61891", "cap", "05.11.2011", 9);
+("4TH8794", "turek", "04.09.1935", 6);
+("8R18941", "sichra", "30.11.2025", 10);
+("8TH8643", "gabryys", "11.05.2018", 7);
+
+insert into OPRAVY(popis, cena, datum, idAuta)
+values
+("Špinavé zrcátko", 100000, "20.12.1851", 18);
+("Rozkoplé dveře", 20, "02.11.1120", 20);
+("Pochcaná sedačka", 80000, "15.02.2024", 17);
+("Křivý volant", 400, "11.09.2001", 16);
+("Okurka ve vyfuku", 10000, "28.02.1954", 19);
