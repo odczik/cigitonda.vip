7dae86e9b3e584a79738065432e36d246e261c85
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2024-12-05 17:03:48 +0100

    a

--- zwa/tetris/script.js
+++ zwa/tetris/script.js
@@ -2,11 +2,14 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-var size
+var size, fallItrv
 var MOVING = {
-    x: 0, y: 0,
+    x: 4, y: -4,
     matrix: SHAPES[SHAPES.all[0]][0],
-    
+
+    left: false, right: false, down: false, rotate: false,
+    fast: false,
+    speed: 500
 }
 Scale()
 function Scale(){
@@ -32,3 +35,85 @@ function DisplayShape(X, Y, shapeMatrix, color){
         }
     }
 }
+function startCycle(){
+
+    setInterval(() => {
+
+        if(MOVING.left) MOVING.x--
+        if(MOVING.right) MOVING.x++
+    
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        DisplayShape(MOVING.x, MOVING.y, MOVING.matrix, 'red')
+
+    }, MOVING.speed/4)
+
+    fallItrv = setInterval(fallInterval, MOVING.speed)
+}
+function DisplayMatrix(){
+    for(let y = 0; y<20; y++){
+        for(let x = 0; x<10; x++){
+
+            if(MATRIX[y][x] != 0){
+                ctx.fillStyle = MATRIX[y][x]
+                ctx.fillRect(x*size, y*size, size, size)
+            }
+        }
+    }
+}
+function fallInterval(){
+    
+    if(MOVING.y < 16){
+        MOVING.y++
+    }else{
+        MOVING.y = 0
+        MOVING.x = 4
+    }
+
+    if(MOVING.down && !MOVING.fast){
+
+        MOVING.fast = true
+        MOVING.speed /= 4
+        clearInterval(fallItrv)
+        fallItrv = setInterval(fallInterval, MOVING.speed)
+    
+    }else if(!MOVING.down && MOVING.fast){
+        MOVING.fast = false
+        MOVING.speed *= 4
+        clearInterval(fallItrv)
+        fallItrv = setInterval(fallInterval, MOVING.speed)
+    }
+}
+window.addEventListener('keydown', e => {
+    switch(e.key.toLowerCase()){
+        case 'a':
+            MOVING.left = true
+            break
+        case 'd':
+            MOVING.right = true
+            break
+        case 's':
+            MOVING.down = true
+            break
+        case 'q':
+            MOVING.rotate = true
+            break
+    }
+})
+
+window.addEventListener('keyup', e => {
+    switch(e.key.toLowerCase()){
+        case 'a':
+            MOVING.left = false
+            break
+        case 'd':
+            MOVING.right = false
+            break
+        case 's':
+            MOVING.down = false
+            break
+        case 'q':
+            MOVING.rotate = false
+            break
+    }
+})
+startCycle()
