e60b71bffb7e1de15b69198b98eb82f2e6567682
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-01-01 19:49:52 +0100

    skap devko

--- zwa/makeItTeirList/script.js
+++ zwa/makeItTeirList/script.js
@@ -1,6 +1,11 @@
 const imgInput = document.getElementById('imgInput'), imgContainer = document.getElementById('picContainer')
 const fileInputInfo = document.getElementById('fileInputInfo')
 
+var PICS = {
+    len: 3,
+    dirList: []
+}
+
 imgInput.addEventListener('change', () => {
     const files = imgInput.files
     imgContainer.innerHTML = ''
@@ -9,17 +14,29 @@ imgInput.addEventListener('change', () => {
         fileInputInfo.innerText('zdane obrazky nebyly nahrany')
         return
     }
-
+    PICS.len = 0
     Array.from(files).forEach(item => {
+    
         if(item.type.startsWith('image/')){
             const reader = new FileReader();
 
             reader.onload = function(e){
+    
                 const img = document.createElement('img')
                 img.src = e.target.result
+                img.id = `img${PICS.len}`
+                
+                PICS.len++
+                PICS.dirList.push(img.src)
+
                 imgContainer.appendChild(img)
             }
             reader.readAsDataURL(item);
         }
     })
+    console.log(PICS)
+})
+
+window.addEventListener('resize', () => {
+    console.log(window.innerWidth, innerHeight)
 })
