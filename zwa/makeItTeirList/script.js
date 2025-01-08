de62efae66ecdfa2ef260eb186843b3671ecfce9
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-01-08 16:27:27 +0100

    tohle se melo stat treba 2 tydny zpatky

--- zwa/makeItTeirList/script.js
+++ zwa/makeItTeirList/script.js
@@ -1,9 +1,12 @@
 const imgInput = document.getElementById('imgInput'), imgContainer = document.getElementById('picContainer')
 const fileInputInfo = document.getElementById('fileInputInfo')
+const cursorImg = document.getElementById('cursorImg'), cursorCont = document.getElementById('cursorCont')
 
 var PICS = {
     len: 3,
-    dirList: []
+    dirList: [],
+    mouseOver: '',
+    hold: false
 }
 
 imgInput.addEventListener('change', () => {
@@ -24,19 +27,48 @@ imgInput.addEventListener('change', () => {
     
                 const img = document.createElement('img')
                 img.src = e.target.result
+      
                 img.id = `img${PICS.len}`
                 
                 PICS.len++
                 PICS.dirList.push(img.src)
 
                 imgContainer.appendChild(img)
+                
+                img.addEventListener('mousedown', e => {
+                    e.preventDefault()
+                })
+                img.addEventListener('mouseover', () => {
+                    PICS.mouseOver = img.id
+                    console.log(img.id)
+                })
+                img.addEventListener('mouseleave', () => {
+                    PICS.mouseOver = ''
+                })
             }
             reader.readAsDataURL(item);
         }
     })
-    console.log(PICS)
 })
+window.addEventListener('mousedown', e => {
+    if(PICS.mouseOver != ''){
+        PICS.hold = PICS.mouseOver
 
-window.addEventListener('resize', () => {
-    console.log(window.innerWidth, innerHeight)
+        console.log(PICS.hold, typeof PICS.hold)
+        console.log(PICS.hold.subString(3))
+        cursorImg.src = PICS.dirList[PICS.hold.subString(3)]
+
+        cursorCont.style.left = `${e.clientX}px`, cursorCont.style.top = `${e.clientY}px`
+    }
+})
+window.addEventListener('mouseup', () => {
+    if(PICS.hold != false){
+        PICS.hold = false
+        cursorCont.style.top = '-100%'
+    }
+})
+window.addEventListener('mousemove', e => {
+    if(PICS.hold != false){
+        cursorCont.style.left = `${e.clientX}px`, cursorCont.style.top = `${e.clientY}px`           
+    }
 })
