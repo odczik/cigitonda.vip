84f6033c6e6e96490ef615744a944454d25f72cd
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-01-21 14:18:08 +0100

    kys ty sasku

---
+++ zwa/php-scripty/21-01-25.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form method="POST">
+        RC
+        <input type="number" id="RC" name="RC" maxlength="10">
+        <input type="submit" value="submit">
+    </form>
+
+    <?php
+        $mesice = ["ledna", "unoru", "brezenu", "dubenu", "kvetenu", "cervnu", "cervence", "srpna", "zari", "rijna", "listopadu", "prosince"];
+    
+        if(isset($_POST["RC"])){
+
+            $rc = $_POST["RC"];
+
+            $y = substr($rc, 0, 2);
+            $m = substr($rc, 2, 2);
+            $d = substr($rc, 4, 2);
+
+            $pohlavi = $m > 50 ? 1 : 0;
+            $m -= 50;
+
+            if($m <= 12 && $d <= 31){
+
+                echo $pohlavi == 1 ? "tato zena se narodila " : "tenhle muz se narodil ";
+                echo $d . ". " . $mesice[$m] . " 20" . $y;
+            }
+        }
+
+    ?>
+
+</body>
+</html>
